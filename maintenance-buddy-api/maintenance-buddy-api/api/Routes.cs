edff87ddd60fffacc6486efbe87c263f5906b579
Georg <Georg.Braun92@gmail.com>
2022-09-02 19:41:35 +0200

    route structure

--- maintenance-buddy-api/maintenance-buddy-api/api/Routes.cs
+++ maintenance-buddy-api/maintenance-buddy-api/api/Routes.cs
@@ -5,9 +5,12 @@ public static class Routes
     private const string Base = "/api";
     private const string Version = "";
 
-    public const string Status = $"{Base}{Version}/Status";
+    // Commands
     public const string CreateVehicle = $"{Base}{Version}/create-vehicle";
     public const string AddActionTemplate = $"{Base}{Version}/add-action-template";
     public const string DeleteActionTemplate = $"{Base}{Version}/delete-action-template";
+    
+    // Queries
+    public const string Status = $"{Base}{Version}/Status";
     public const string ActionTemplateQuery = $"{Base}{Version}/get-action-templates";
 }
