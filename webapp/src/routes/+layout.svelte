2918aadcecf7ee8524c70e90e79efc8df1a90d92
Georg <Georg.Braun92@gmail.com>
2022-09-19 18:13:04 +0200

    add authentication in client app

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import auth from '../auth-service';
+
+
+
+	onMount(async () => {
+		console.log('Mounting app');
+		await auth.createClient();
+	});
+
+	function login() {
+		auth.loginWithPopup();
+	}
+
+	function logout() {
+		auth.logout();
+	}
+
+	let { isAuthenticated, user } = auth;
 </script>
 
 <Header />
 
 <main>
+	<div class="my-auto ml-2">
+		{#if $isAuthenticated}
+			<span>{$user.name} ({$user.email})</span>
+		{:else}<span>Not logged in</span>{/if}
+
+		<span>
+			{#if $isAuthenticated}
+				<a href="/#" on:click={logout}>Log Out</a>
+			{:else}
+				<a href="/#" on:click={login}>Log In</a>
+			{/if}
+		</span>		
+	</div>
+
 	<slot />
 </main>
 
