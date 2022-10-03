9d0fb9e85daf45b3215351b84dbbc2591f6b1e1f
Georg <Georg.Braun92@gmail.com>
2022-10-03 13:08:30 +0200

    add domain project

--- maintenance-buddy-api/maintenance-buddy-api-domain/domain/vehicle/ActionTemplate.cs
+++ maintenance-buddy-api/maintenance-buddy-api-domain/domain/vehicle/ActionTemplate.cs
@@ -108,23 +108,3 @@ public class ActionTemplate
         return PendingAction.Create(Id, timespanTillAction, kilometerTillAction);
     }
 }
-
-public class PendingAction
-{
-    public Guid ActionTemplateId { get; init; }
-    public TimeSpan TimeTillAction { get; init; }
-    public int KilometerTillAction { get; init; }
-    public bool Exceeded => TimeTillAction.Ticks < 0 || KilometerTillAction < 0;
-
-    private PendingAction(){}
-
-    public static PendingAction Create(Guid actionTemplateId, TimeSpan timeTillAction, int kilometer)
-    {
-        return new PendingAction
-        {
-            ActionTemplateId = actionTemplateId,
-            TimeTillAction = timeTillAction,
-            KilometerTillAction = kilometer
-        };
-    }
-}
