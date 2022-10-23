89d859923f18c852d79ef133ded8655b2156b6da
Georg <Georg.Braun92@gmail.com>
2022-10-23 16:36:04 +0200

    redesign vehicle create form

--- webapp/src/routes/vehicles/create-vehicle/+page.svelte
+++ webapp/src/routes/vehicles/create-vehicle/+page.svelte
@@ -6,14 +6,17 @@
 
 <div class="">
 	<h1>Add a new vehicle</h1>
-	<div class="">
-		<input placeholder="BMW" class="w-96 bg-slate-50" bind:value={name} />
+	
+	<div class="grid grid-cols-2 w-1/2">
+		<div>Name</div>
+		<input placeholder="BMW" class="w-32 bg-slate-50" bind:value={name} />
+		<div>Kilometer</div>
 		<input step="1" class="w-32 bg-slate-50" type="number" bind:value={kilometer} />
 	</div>
 
 	<div class="my-auto">
 		<button
-			class="rounded px-2 ml-4  my-auto bg-slate-200"
+			class="rounded px-2 my-auto bg-slate-200"
 			on:click={async () => {
 				await createVehicle(name, kilometer);
 			}}
